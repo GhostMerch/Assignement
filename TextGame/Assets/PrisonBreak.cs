58955219b50559a36f5cad16253eb412ca280223
Kedron Mallia <a100923@mcast.org.mt>
2017-10-17 09:12:45 +0200

    update 1

--- TextGame/Assets/PrisonBreak.cs
+++ TextGame/Assets/PrisonBreak.cs
@@ -28,6 +28,14 @@ public class PrisonBreak : MonoBehaviour {
         {
             myStates = States.sheets_0;
         }
+        else if (Input.GetKeyDown(KeyCode.M))
+        {
+            myStates = States.mirror;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            myStates = States.lock_0;
+        }
     }
 
     void sheets_0()
@@ -40,6 +48,93 @@ public class PrisonBreak : MonoBehaviour {
             myStates = States.cell;
         }
     }
+
+    void mirror()
+    {
+        myText.text = "You are still in your cell, and you STILL want to escape! There are " +
+    "some dirty sheets on the bed, a mirror , " +
+    "and that pesky door is still there, and firmly locked!\n\n" +
+    "Press R to go back Cell or T to Take the Mirror";
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myStates = States.cell;
+        }
+        else if (Input.GetKeyDown(KeyCode.T))
+        {
+            myStates = States.cell_mirror;
+        }
+    }
+
+    void cell_mirror()
+
+    {
+        myText.text = "You are still in your cell, and you STILL want to escape! There are " +
+                      "some dirty sheets on the bed, a mark where the mirror was, " +
+                      "and that pesky door is still there, and firmly locked!\n\n" +
+                      "Press S to view Sheets, or L to view Lock";
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            myStates = States.sheets_1;
+        }
+        else if (Input.GetKeyDown(KeyCode.L))
+        {
+            myStates = States.lock_1;
+        }
+    }
+
+    void sheets_1()
+    {
+        myText.text = "Holding a mirror in your hand doesn't make the sheets look " +
+                     "any better.\n\n" +
+                     "Press R to Return to roaming your cell";
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myStates = States.cell_mirror;
+        }
+    }
+
+    void lock_1()
+    {
+        myText.text = "You carefully put the mirror through the bars, and turn it round " +
+      "so you can see the lock. You can just make out fingerprints around " +
+      "the buttons. You press the dirty buttons, and hear a click.\n\n" +
+      "Press O to Open, or R to Return to your cell";
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myStates = States.cell_mirror;
+        }
+        else if (Input.GetKeyDown(KeyCode.O))
+        {
+            myStates = States.freedom;
+        }
+    }
+
+    void freedom()
+    {
+        myText.text = "YOU ARE FREE!! " +
+		"Press P to Play again." ;
+
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            myStates = States.start;
+        }
+    }
+
+    void lock_0()
+    {
+       myText.text = "This is one of those button locks. You have no idea what the " +
+      "combination is. You wish you could somehow see where the dirty " +
+    "fingerprints were, maybe that would help.\n\n" +
+    "Press R to Return to roaming your cell";
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            myStates = States.cell;
+        }
+    }
+
 
     private enum States
     {
@@ -69,6 +164,30 @@ public class PrisonBreak : MonoBehaviour {
         else if(myStates == States.sheets_0)
         {
             sheets_0();
+        }
+        else if (myStates == States.mirror)
+        {
+            mirror();
+        }
+        else if (myStates == States.lock_0)
+        {
+            lock_0();
+        }
+        else if (myStates == States.cell_mirror)
+        {
+            cell_mirror();
+        }
+        else if (myStates == States.lock_1)
+        {
+            lock_1();
+        }
+        else if (myStates == States.sheets_1)
+        {
+            sheets_1();
+        }
+        else if (myStates == States.freedom)
+        {
+            freedom();
         }
 	}
 }
